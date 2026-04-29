4c22351ee517c1dfe0df20d5b5a2caca498f6415
Mario <sherifmario18@gmail.com>
2026-04-29 21:44:51 +0300

    Refactor risk level evaluation logic in main.js
    Reorganized risk level conditions for clarity and accuracy.

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-let FailedUserlogin = 4
-let UserAge = 16
+let FailedUserlogin = 4;
+let UserAge = 16;
 
-if (FailedUserlogin < 3){
-    console.log("Low Risk Level");
-}
-else if (FailedUserlogin > 3 && UserAge < 18){
-console.log("critical Risk Level")
-}
-else if (FailedUserlogin === 3 || FailedUserlogin=== 4 || FailedUserlogin === 5){
-    console.log("Medium Risk Level");
-}
-else if (FailedUserlogin > 5){
-    console.log("High Risk Level");
+if (FailedUserlogin > 3 && UserAge < 18) {
+  console.log("critical Risk Level");
+} else if (FailedUserlogin > 5) {
+  console.log("High Risk Level");
+} else if (
+  FailedUserlogin === 3 ||
+  FailedUserlogin === 4 ||
+  FailedUserlogin === 5
+) {
+  console.log("Medium Risk Level");
+} else if (FailedUserlogin < 3) {
+  console.log("Low Risk Level");
 }
